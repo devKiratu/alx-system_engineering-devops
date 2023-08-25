e442648fec35502737a878244a4cb35fd61d16b5
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-25 12:59:23 +0300

    add 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This manifest creates a file `school` in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
