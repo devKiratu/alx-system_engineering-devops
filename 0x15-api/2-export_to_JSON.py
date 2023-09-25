46e69fccdcd7b719abc01040896bc322b3f92236
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-25 13:05:32 +0300

    refactor 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module uses the requests module to get information about an employee's
-todo data
+todo data and exports it to a json file
 """
 import json
 import requests
@@ -12,21 +12,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         user_id = sys.argv[1]
         user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-        res = requests.get(user_url)
-        user_data = res.json()
-        user_name = user_data.get("name")
+        user_data = requests.get(user_url).json()
 
         todos_url = "https://jsonplaceholder.typicode.com/todos"
-        res = requests.get(todos_url, params={'userId': sys.argv[1]})
-        todos = res.json()
-        completed_todos = list(filter(
-            lambda todo: todo.get("completed"), todos))
-        output = "Employee {} is done with tasks({}/{}):".format(
-                user_name, len(completed_todos), len(todos)
-                )
-        print(output)
-        for todo in completed_todos:
-            print("\t {}".format(todo.get("title")))
+        todos = requests.get(todos_url, params={'userId': user_id}).json()
 
         # export json data
         jsonfile = "{}.json".format(user_id)
@@ -34,9 +23,9 @@ if __name__ == "__main__":
         formatted_todos = []
         for todo in todos:
             obj = {}
-            obj["task"] = "{}".format(todo.get('title'))
+            obj["task"] = todo.get('title')
             obj["completed"] = todo.get('completed')
-            obj["username"] = "{}".format(user_data.get('username'))
+            obj["username"] = user_data.get('username')
             formatted_todos.append(obj)
         user_obj[user_id] = formatted_todos
         with open(jsonfile, 'w', encoding='utf-8') as f:
