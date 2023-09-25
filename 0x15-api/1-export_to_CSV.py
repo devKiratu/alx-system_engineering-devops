13eca794471b36d23b502d4a482bba2834aaf7e3
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-25 13:00:35 +0300

    refactor 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module uses the requests module to get information about an employee's
-todo data
+todo data and exports it to csv
 """
 import requests
 import sys
@@ -9,25 +9,15 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        user_id = sys.argv[1]
         user_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-        res = requests.get(user_url)
-        user_data = res.json()
-        user_name = user_data.get("name")
+        user_data = requests.get(user_url).json()
 
         todos_url = "https://jsonplaceholder.typicode.com/todos"
-        res = requests.get(todos_url, params={'userId': sys.argv[1]})
-        todos = res.json()
-        completed_todos = list(filter(
-            lambda todo: todo.get("completed"), todos))
-        output = "Employee {} is done with tasks({}/{})".format(
-                user_name, len(completed_todos), len(todos)
-                )
-        print(output)
-        for todo in completed_todos:
-            print("\t {}".format(todo.get("title")))
+        todos = requests.get(todos_url, params={'userId': user_id}).json()
 
         # export data to csv
-        filename = "{}.csv".format(sys.argv[1])
+        filename = "{}.csv".format(user_id)
         with open(filename, 'a', encoding='utf-8') as f:
             for todo in todos:
                 line = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
