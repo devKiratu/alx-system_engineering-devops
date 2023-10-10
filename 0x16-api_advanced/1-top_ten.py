77618239b180e927fbe6bcd9d66e0dc8e15c6e44
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-10-10 12:15:18 +0300

    prevent redirects in 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;\
                 rv:109.0) Gecko/20100101 Firefox/118.0'}
-    res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     posts = res.json().get('data', {}).get('children', None)
     if posts is None:
         print("None")
