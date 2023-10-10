d477595ce6203357a0f23e65a11ae417f2c63a8a
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-10-10 12:38:39 +0300

    refactor recursive function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,6 +2,7 @@
 """This script queries the reddit api for a given subreddit"""
 
 import requests
+after = None
 
 
 def recurse(subreddit, hot_list=[]):
@@ -9,21 +10,10 @@ def recurse(subreddit, hot_list=[]):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;\
                 rv:109.0) Gecko/20100101 Firefox/118.0'}
+    global after
+    params = {'after': after}
 
-    def get_more_posts(after):
-        if after is None:
-            return hot_list
-        res = requests.get(url, headers=headers, params={'after': after})
-        data = res.json().get('data', {})
-        posts = data.get('children', None)
-        if posts is None:
-            return None
-        for post in posts:
-            hot_list.append(post.get('data', {}).get('title'))
-        after = data.get('after')
-        return get_more_posts(after)
-
-    res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers, params=params)
     data = res.json().get('data', {})
     posts = data.get('children', None)
     if posts is None:
@@ -31,4 +21,6 @@ def recurse(subreddit, hot_list=[]):
     for post in posts:
         hot_list.append(post.get('data', {}).get('title'))
     after = data.get('after')
-    return get_more_posts(after)
+    if after is None:
+        return hot_list
+    return recurse(subreddit)
