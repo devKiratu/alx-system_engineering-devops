f82bee7e7a472c2b5555c8fa278d0f93a6a84ff4
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-10-19 21:58:42 +0300

    add 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# This script enables nginx to serve a peak load
+
+exec { 'increase_ulimit':
+  command => '/bin/sed -i "s/15/4096" /etc/default/nginx'
+}
+
+exec { 'restart_nginx':
+  command => '/usr/sbin/service nginx restart'
+}
