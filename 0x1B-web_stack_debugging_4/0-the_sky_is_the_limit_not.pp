83ddd9705ee3472405359d7b18f711388ac166d5
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-10-19 22:03:42 +0300

    fix error in puppet script

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # This script enables nginx to serve a peak load
 
 exec { 'increase_ulimit':
-  command => '/bin/sed -i "s/15/4096" /etc/default/nginx'
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx'
 }
 
 exec { 'restart_nginx':
